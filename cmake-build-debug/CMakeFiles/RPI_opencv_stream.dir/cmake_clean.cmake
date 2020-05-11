6474d4f5da2ff6a1088ac828e51b1aefd3c41aa2
valdis <valdger@gmail.com>
2020-05-11 23:48:29 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/RPI_opencv_stream.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPI_opencv_stream.dir/main.c.obj"
+  "RPI_opencv_stream.exe"
+  "RPI_opencv_stream.exe.manifest"
+  "RPI_opencv_stream.pdb"
+  "libRPI_opencv_stream.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RPI_opencv_stream.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
